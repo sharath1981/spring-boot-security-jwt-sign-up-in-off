8d5be7bbf8f4747e46190b0bfcdfd72872a01070
SHARATH KUMAR B <sharath1981@gmail.com>
2021-05-21 17:51:44 +0530

    Refactored

--- src/main/java/com/svh/springbootsecurityjwt/constant/AppConstants.java
+++ src/main/java/com/svh/springbootsecurityjwt/constant/AppConstants.java
@@ -1,6 +1,6 @@
 package com.svh.springbootsecurityjwt.constant;
 
-public class AppConstants {
+public final class AppConstants {
 
     public static final long JWT_VALIDITY = 5 * 60 * 60 * 1000;
 	public static final String BEARER = "Bearer ";
